ca1ac6236ab4deb3578b136ac53f947e8ba667c2
Matheus Carregal <matheuschabib@gmail.com>
2020-08-28 18:08:32 -0300

    Update main.js Validando Formulário

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ function getTotal(list){
     for(var key in list){
         total += list[key].value * list[key].amount;
     }
-    return total;
+    document.getElementById("totalValue").innerHTML = formatValue(total);
 }
 
 //essa função criou os valores da tabela, criou a variavel table onde recebia o cabeçalho da tabela, e depois
@@ -18,10 +18,11 @@ function getTotal(list){
 function setList(list){
     var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
     for(var key in list){
-        table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td> <button class="btn btn-default" onclick = "setUpdate('+key+');">Edit</button> | <button class="btn btn-default" onclick = "deleteData('+key+');">Delete</button></td></tr>';
+        table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ formatAmount(list[key].amount) +'</td><td>'+ formatValue(list[key].value) +'</td><td> <button class="btn btn-default" onclick = "setUpdate('+key+');">Edit</button> | <button class="btn btn-default" onclick = "deleteData('+key+');">Delete</button></td></tr>';
     }
     table += '</tbody>';
     document.getElementById("listTable").innerHTML = table;
+    getTotal(list);
 }
 
 //formatando o desc para ter uma letra maiuscula na frente
@@ -31,6 +32,10 @@ function formatDesc(desc){
     //a partir da região especifica, no caso por exemplo de "rice" o slice(1) iria pegar somente o "ice"
     str = str.charAt(0).toUpperCase() + str.slice(1);
     return str;
+}
+
+function formatAmount(amount){
+    return parseInt(amount);
 }
 
 //formatando o value para ter virgula em vez de ponto, para ter so 2 casas decimais e ter um cifrão na frente
@@ -46,6 +51,9 @@ function formatValue(value){
 
 //adicionando elementos na tabela
 function addData(){
+    if(!validation()){
+        return;
+    }
     var desc = document.getElementById("desc").value;
     var amount = document.getElementById("amount").value;
     var value = document.getElementById("value").value;
@@ -76,10 +84,14 @@ function resetForm(){
     document.getElementById("btnAdd").style.display = "inline-block";
 
     document.getElementById("inputIdUpdate").innerHTML = "";
+    document.getElementById("errors").style.display = "none";
 }
 
 //ao apertar o botão salvar, irá tirar os valores dos inputs e atualizar na tabela
 function updateData(){
+    if(!validation()){
+        return;
+    }
     var id = document.getElementById("idUpdate").value;
     var desc = document.getElementById("desc").value;
     var amount = document.getElementById("amount").value;
@@ -108,6 +120,38 @@ function deleteData(id){
     }
 }
 
-setList(list);
+function validation(){
+    var desc = document.getElementById("desc").value;
+    var amount = document.getElementById("amount").value;
+    var value = document.getElementById("value").value;
+    var errors = "";
+    document.getElementById("errors").style.display = "none";
+    if(desc === ""){
+        errors += '<p>Fill out description</p>';
+    }
+    if(amount === ""){
+        errors += '<p>Fill out a quantity</p>';
+    }else if(amount != parseInt(amount)){
+        errors += '<p>Fill out a valid amount</p>';
+    }
+    if(value === ""){
+        errors += '<p>Fill out a value</p>';
+    }else if(value != parseFloat(value)){
+        errors += '<p>Fill out a valid value</p>';
+    }
 
-console.log(getTotal(list));
+    if(errors != ""){
+        document.getElementById("errors").style.display = "block";   
+        document.getElementById("errors").style.backgroundColor = "rgba(85, 85, 85, 0.3)";
+        document.getElementById("errors").style.color = "white";
+        document.getElementById("errors").style.padding = "10px";
+        document.getElementById("errors").style.margin = "10px";
+        document.getElementById("errors").style.borderRadius = "13px";   
+        document.getElementById("errors").innerHTML = "<h3>Error:</h3>" + errors;
+        return 0;
+    }else{
+        return 1;
+    }
+}
+
+setList(list);
